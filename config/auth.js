1f38a5a5cc1fa90378a53c2ec8fa88086d8aaa36
santhoshmani888 <31495133+santhoshmani888@users.noreply.github.com>
2017-11-27 20:17:49 -0600

    config and db file

---
+++ config/auth.js
@@ -0,0 +1,15 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+
+    'twitterAuth' : {
+        'consumerKey'        : 'EK5wP6mpzcYrp1Vipkdm18hHw',
+        'consumerSecret'     : 'wJWNSw8rFExvqr02aX9tTNvXWZspuOa61elcDz5N1be1XopCeS',
+        'callbackURL'        : 'http://localhost:3000/auth/twitter/callback'
+    },
+
+   
+
+};
